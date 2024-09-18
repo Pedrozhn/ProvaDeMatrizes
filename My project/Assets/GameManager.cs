1d991759d1424f2bd1755f10a7194783664724ed
NicolasXd36MlM <yukiminko519@gmail.com>
2024-09-18 11:11:21 -0300

    criação de variáveis

--- My project/Assets/GameManager.cs
+++ My project/Assets/GameManager.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject jogador1;
+    public GameObject jogador2;
+    public int linha = 5;
+    public int coluna = 5;
+    public float espacamento = 1.1f;
+
+    private int[] Bloco;
+    private int[] Conquista;
+    
+  
+
     // Start is called before the first frame update
     void Start()
     {
